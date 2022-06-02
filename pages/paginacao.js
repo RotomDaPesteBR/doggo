654fadbb17d804ca0ca9234a928662b5e131ce89
Rotom da Peste - BR <mielsen.gamer2005@gmail.com>
2022-06-02 08:19:09 -0300

    paginação fix

--- pages/paginacao.js
+++ pages/paginacao.js
@@ -52,7 +52,7 @@ export default function Home(){
     }
 
     function changePage(number) {
-        pageNumber = number;
+        pageNumber = number + 1;
         setPage(pageNumber);
         updateImgUrl();
     }
